38485d7df4d5dda3f42a888c415c4be2860bf0fa
Maciej Gomółka <mgomolka92@gmail.com>
2020-10-04 17:42:58 +0200

    Add animation description initializer

--- Sources/MGFlipView/AnimationDescription.swift
+++ Sources/MGFlipView/AnimationDescription.swift
@@ -1,6 +1,13 @@
 public struct AnimationDescription {
+
+  public init(type: AnimationType, duration: Double) {
+    self.type = type
+    self.duration = duration
+  }
+
   let type: AnimationType
   let duration: Double
+
 }
 
 public extension AnimationDescription {
