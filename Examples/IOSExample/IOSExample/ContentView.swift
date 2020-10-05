8cbc10a7c5efd1325a80f8cb74bbe5a8071688be
Maciej Gomółka <mgomolka92@gmail.com>
2020-10-05 22:24:02 +0200

    Add example view

--- Examples/IOSExample/IOSExample/ContentView.swift
+++ Examples/IOSExample/IOSExample/ContentView.swift
@@ -2,13 +2,105 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    GameView(itemSize: .init(width: 70, height: 70), fontSize: 20)
   }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
 }
+#endif
+
+struct Offset: Identifiable {
+  let id = UUID()
+  let x: CGFloat
+  let y: CGFloat
+}
+
+struct GameView: View {
+
+  init(itemSize: CGSize, fontSize: CGFloat) {
+    self.itemSize = itemSize
+    self.fontSize = fontSize
+  }
+
+  let itemSize: CGSize
+  let fontSize: CGFloat
+
+  var body: some View {
+    GeometryReader { geometry in
+      ForEach(generateOffsets(width: geometry.size.width, height: geometry.size.height)) { offset in
+        ItemView(itemSize: itemSize, fontSize: 20, emoji: allEmojis.first!)
+          .offset(x: offset.x, y: offset.y)
+      }
+    }.padding()
+  }
+
+  func generateOffsets(width: CGFloat, height: CGFloat) -> [Offset] {
+    let numberOfColumns = Int(width / itemSize.width)
+    let numberOfRows = Int(height / itemSize.height)
+    var numberOfItems = numberOfColumns * numberOfRows
+    numberOfItems = numberOfItems % 2 == 0 ? numberOfItems : numberOfItems - 1
+
+    let horizaontalSpacing = width / (CGFloat(numberOfColumns) * itemSize.width)
+    let verticalSpacing = height / (CGFloat(numberOfRows) * itemSize.height)
+
+    let startSpacingColumn = (width - (CGFloat(numberOfColumns) * itemSize.width + CGFloat(max(numberOfColumns, numberOfColumns - 1)) * horizaontalSpacing)) / 2
+    let startSpacingRow = (height - (CGFloat(numberOfRows) * itemSize.height + CGFloat(max(numberOfRows, numberOfRows - 1)) * verticalSpacing)) / 2
+
+    var offsets: [Offset] = []
+
+    for column in 0..<numberOfColumns {
+      for row in 0..<numberOfRows {
+        if numberOfItems - 1 >= offsets.count {
+          let offsetX = (CGFloat(column) * itemSize.width + CGFloat(max(column, column - 1)) * horizaontalSpacing) + startSpacingColumn
+          let offsetY = CGFloat(row) * itemSize.height + CGFloat(max(row, row - 1)) * verticalSpacing + startSpacingRow
+          offsets.append(.init(x: offsetX, y: offsetY))
+        }
+      }
+    }
+
+    return offsets
+  }
+
+}
+
+import MGFlipView
+
+struct ItemView: View {
+
+  init(itemSize: CGSize, fontSize: CGFloat, emoji: Character) {
+    self.itemSize = itemSize
+    self.fontSize = fontSize
+    self.emoji = emoji
+  }
+
+  let itemSize: CGSize
+  let fontSize: CGFloat
+  let emoji: Character
+
+  @State var flipped = false
+
+  var body: some View {
+    FlipView(frontView: {
+      Color.yellow
+        .frame(width: itemSize.width, height: itemSize.height)
+        .cornerRadius(min(itemSize.height, itemSize.width) * 0.25)
+    }, backView: {
+      Text(String(emoji))
+        .font(.system(size: fontSize))
+        .frame(width: itemSize.width, height: itemSize.height)
+        .background(Color.yellow)
+        .cornerRadius(min(itemSize.height, itemSize.width) * 0.25)
+    }, fliped: $flipped)
+      .onTapGesture {
+        flipped.toggle()
+      }
+  }
+
+}
+
+let allEmojis = "🧳🌂☂️🧵🧶👓🕶🥽🥼🦺👔👕👖🧣🧤🧥🧦👗👘🥻🩱🩲🩳👙👚👛👜👝🎒👞👟🥾🥿👠👡🩰👢👑👒🎩🎓🧢⛑💄💍💼"
--- Examples/IOSExample/IOSExample/IOSExampleApp.swift
+++ Examples/IOSExample/IOSExample/IOSExampleApp.swift
@@ -1,10 +1,105 @@
 import SwiftUI
+import MGFlipView
 
 @main
 struct IOSExampleApp: App {
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ExampleView(itemSize: .init(width: 120, height: 120))
     }
   }
+
+}
+
+struct ExampleView: View {
+
+  init(itemSize: CGSize) {
+    self.itemSize = itemSize
+  }
+
+  let itemSize: CGSize
+
+  @State var flipped: Bool = false
+
+  var body: some View {
+    VStack {
+      HStack {
+        flipView(flipAxis: .x, animationType: .easeIn)
+        flipView(flipAxis: .y, animationType: .easeOut)
+      }
+      HStack {
+        flipView(flipAxis: .xy, animationType: .easeInOut)
+        flipView(flipAxis: .custom(x: 0.1, y: 0.5), animationType: .linear)
+      }
+      FlipView(frontView: {
+        Text("?")
+          .foregroundColor(.white)
+          .font(.system(size: 30))
+          .frame(width: itemSize.width, height: itemSize.height)
+          .background(Color.gray)
+          .mask(Circle())
+      }, backView: {
+        Image("1")
+          .resizable()
+          .frame(width: itemSize.width, height: itemSize.height)
+          .mask(Circle())
+      }, fliped: $flipped, animation: .init(type: .linear, duration: 0.25))
+      Button(action: { flipped.toggle() }) {
+        Text("Flip!")
+      }
+    }
+  }
+
+  func flipView(flipAxis: FlipAxis, animationType: AnimationType) -> some View {
+    FlipView(frontView: { cardView(front: true, flipAxis: flipAxis, animationType: animationType) },
+             backView: { cardView(front: false, flipAxis: flipAxis, animationType: animationType) },
+             fliped: $flipped, flipAxis: flipAxis, animation: .init(type: animationType, duration: 2))
+  }
+
+  func cardView(front: Bool, flipAxis: FlipAxis, animationType: AnimationType) -> some View {
+    var cardSide = front ? "Front\n" : "Back\n"
+    cardSide += "axis: \(flipAxis.humanReadable)\n"
+    cardSide += "anim: \(animationType.humanReadable)"
+
+    return Text(cardSide)
+      .frame(width: itemSize.width, height: itemSize.height)
+      .background(Color.yellow)
+      .cornerRadius(min(itemSize.width, itemSize.height) * 0.25)
+  }
+
+}
+
+private extension FlipAxis {
+
+  var humanReadable: String {
+    switch self {
+    case .x:
+      return "x"
+    case .y:
+      return "y"
+    case .xy:
+      return "xy"
+    case .custom(let x, let y):
+      return "x:\(x),y:\(y)"
+    }
+  }
+
+}
+
+private extension AnimationType {
+
+  var humanReadable: String {
+    switch self {
+    case .linear:
+      return "linear"
+    case .easeIn:
+      return "easeIn"
+    case .easeOut:
+      return "easeOut"
+    case .easeInOut:
+      return "easeInOut"
+    }
+  }
+
 }
