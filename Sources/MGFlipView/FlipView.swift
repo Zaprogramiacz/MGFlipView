05a5eb179e3d5ee9da16f8242e675ef9ac2aa706
Maciej Gomółka <mgomolka92@gmail.com>
2020-10-06 00:38:03 +0200

    Update animations

--- Sources/MGFlipView/FlipView.swift
+++ Sources/MGFlipView/FlipView.swift
@@ -25,10 +25,16 @@ public struct FlipView<ContentFront: View, ContentBack: View>: View {
     ZStack {
       frontView()
         .rotation3DEffect(.init(radians: fliped ? (.pi / 2).closeValue : 0.closeValue), axis: flipAxis.value)
-        .animation(animation.type.front(duration: animation.duration / 2).delay(fliped ? 0 : animation.duration / 2))
+        .animation(
+          animation.type.front(duration: animation.duration / 2, reversed: !fliped)
+            .delay(fliped ? 0 : animation.duration / 2)
+        )
       backView()
         .rotation3DEffect(.init(radians: fliped ? 0.closeValue : (-.pi / 2).closeValue), axis: flipAxis.value)
-        .animation(animation.type.back(duration: animation.duration / 2).delay(fliped ? animation.duration / 2 : 0))
+        .animation(
+         animation.type.back(duration: animation.duration / 2, reversed: !fliped)
+          .delay(fliped ? animation.duration / 2 : 0)
+        )
     }
   }
 
@@ -36,21 +42,29 @@ public struct FlipView<ContentFront: View, ContentBack: View>: View {
 
 private extension AnimationType {
 
-  func front(duration: Double) -> Animation {
+  func front(duration: Double, reversed: Bool) -> Animation {
     switch self {
-    case .linear, .easeOut:
+    case .linear:
       return .linear(duration: duration)
-    case .easeIn, .easeInOut:
-      return .easeIn(duration: duration)
+    case .easeOut:
+      return reversed ? .easeOut(duration: duration) : .linear(duration: duration)
+    case .easeIn:
+      return reversed ? .linear(duration: duration) : .easeIn(duration: duration)
+    case .easeInOut:
+      return reversed ? .easeOut(duration: duration) : .easeIn(duration: duration)
     }
   }
 
-  func back(duration: Double) -> Animation {
+  func back(duration: Double, reversed: Bool) -> Animation {
     switch self {
-    case .linear, .easeIn:
+    case .linear:
       return .linear(duration: duration)
-    case .easeOut, .easeInOut:
-      return .easeOut(duration: duration)
+    case .easeIn:
+      return reversed ? .easeIn(duration: duration) : .linear(duration: duration)
+    case .easeOut:
+      return reversed ? .linear(duration: duration) : .easeOut(duration: duration)
+    case .easeInOut:
+      return reversed ? .easeIn(duration: duration) : .easeOut(duration: duration)
     }
   }
 
